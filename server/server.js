57bb6e47c24a00896fc38429fa45e83469ff9f6d
Faiz Mohammed <faizmohammed178@gmail.com>
2017-03-14 10:45:21 -0700

    reverted to with node_modules and made some changes in server and package.json

--- server/server.js
+++ server/server.js
@@ -3,15 +3,15 @@ const app = express();
 const morgan = require('morgan');
 const parser = require('body-parser');
 const path = require('path');
-
+const port = process.env.PORT || 8080;
 app.use(morgan('dev'));
 app.use(parser.json());
 
 // app.use(express.static(__dirname + '/../compiled/'));
 app.use(express.static(__dirname + '/../public/'));
 
-app.listen(8080);
-console.log('Listening on ', 8080);
+app.listen(port);
+console.log('Listening on ', port);
 // app.get('/', function(req, res){
 //   res.sendFile(path.join(__dirname, '../client', 'index.html'));
 // });
